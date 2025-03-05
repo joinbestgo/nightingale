bd2751297a60ca75b4b8a3207f3d51c6be6328a8
Qiao, Ying <ying.qiao@mercedes-benz.com>
2025-03-05 14:27:48 +0800

    重构 fe.sh，注释掉下载和解压 n9e-fe 的逻辑，并添加清理和复制 pub 目录的步骤

--- fe.sh
+++ fe.sh
@@ -2,19 +2,22 @@
 
 cp -f ./docker/initsql/a-n9e.sql n9e.sql
 
-if [ ! -d "./pub" ]; then
-    TAG=$(curl -sX GET https://api.github.com/repos/n9e/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
 
-    if ! curl -o n9e-fe-${TAG}.tar.gz -L https://github.com/n9e/fe/releases/download/${TAG}/n9e-fe-${TAG}.tar.gz; then
-        echo "failed to download n9e-fe-${TAG}.tar.gz!"
-        exit 1
-    fi
+# if [ ! -d "./pub" ]; then
+#     TAG=$(curl -sX GET https://api.github.com/repos/n9e/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
 
-    if ! tar zxf n9e-fe-${TAG}.tar.gz; then
-        echo "failed to untar n9e-fe-${TAG}.tar.gz!"
-        exit 2
-    fi
-fi
+#     if ! curl -o n9e-fe-${TAG}.tar.gz -L https://github.com/n9e/fe/releases/download/${TAG}/n9e-fe-${TAG}.tar.gz; then
+#         echo "failed to download n9e-fe-${TAG}.tar.gz!"
+#         exit 1
+#     fi
+
+#     if ! tar zxf n9e-fe-${TAG}.tar.gz; then
+#         echo "failed to untar n9e-fe-${TAG}.tar.gz!"
+#         exit 2
+#     fi
+# fi
+rm -rf ./pub
+cp -a ../nightingale-fe/pub ./pub
 
 GOPATH=$(go env GOPATH)
 GOPATH=${GOPATH:-/home/runner/go}
